08c2675bce5e6cdb61fb793edd5f59f27b5edbea
hyejeong <withoo0414@gmail.com>
2022-02-23 01:31:20 +0900

    improved ui

--- src/pages/details/Detail.jsx
+++ src/pages/details/Detail.jsx
@@ -1,27 +1,23 @@
-import React from "react"
-import ProductsCard from "./components/productCard/ProductsCard"
-import Header from "./components/header/Header"
-import styled from "styled-components"
+import React from "react";
+import ProductsCard from "./components/productCard/ProductsCard";
+import Header from "./components/header/Header";
+import styled from "styled-components";
 
 const Detail = () => {
   return (
-    <DetailContainer>
+    <DetailComponent>
       <Header />
       <ProductsContainer>
         <ProductsCard />
         <ProductsCard />
         <ProductsCard />
       </ProductsContainer>
-    </DetailContainer>
-  )
-}
+    </DetailComponent>
+  );
+};
 
-const DetailContainer = styled.div`
-  width: 500px;
-`
+const DetailComponent = styled.div`width: 100%;`;
 
-const ProductsContainer = styled.div`
-  padding-top: 75px;
-`
+const ProductsContainer = styled.div`padding-top: 75px;`;
 
-export default Detail
+export default Detail;
--- src/pages/details/components/comments/section/comment.jsx
+++ src/pages/details/components/comments/section/comment.jsx
@@ -4,13 +4,13 @@ import styled from "styled-components";
 const Comment = props => {
   return (
     <CommentComponent>
-      <div>
+      <CommentContent>
         <span>member Id</span>
         <div>구매 후 바로 취소해도 반송료 받던데...</div>
-      </div>
-      <div>
+      </CommentContent>
+      <TimeLapse>
         <span>52주</span>
-      </div>
+      </TimeLapse>
     </CommentComponent>
   );
 };
@@ -21,24 +21,27 @@ const CommentComponent = styled.li`
   list-style: none;
   width: 100%;
   margin-bottom: 1.2rem;
-  & div:nth-child(1) {
-    font-size: 1.2rem;
-    font-weight: normal;
-    line-height: 1.5;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    span {
-      font-size: 1.3rem;
-      font-weight: bold;
-      letter-spacing: -0.025rem;
-      margin-right: 0.6rem;
-    }
+`;
+
+const CommentContent = styled.div`
+  font-size: 1.2rem;
+  font-weight: normal;
+  line-height: 1.5;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  span {
+    font-size: 1.3rem;
+    font-weight: bold;
+    letter-spacing: -0.025rem;
+    margin-right: 0.6rem;
   }
-  & div:nth-child(2) {
-    span {
-      margin-top: 0.4rem;
-      color: #999;
-    }
+`;
+
+const TimeLapse = styled.div`
+  line-height: 1.5;
+  span {
+    margin-top: 0.4rem;
+    color: #999;
   }
 `;
