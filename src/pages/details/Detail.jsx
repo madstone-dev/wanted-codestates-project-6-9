3baa8656e1efbdff4e7eee21a7a883c7ee8f32dd
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2022-02-23 09:56:19 +0900

    refactor: components 폴더 이동

--- src/pages/details/Detail.jsx
+++ src/pages/details/Detail.jsx
@@ -1,6 +1,6 @@
 import React from "react"
-import ProductsCard from "./components/productCard/ProductsCard"
-import Header from "./components/header/Header"
+import ProductsCard from "../../components/productCard/ProductsCard"
+import Header from "../../components/header/Header"
 import styled from "styled-components"
 
 const Detail = () => {
