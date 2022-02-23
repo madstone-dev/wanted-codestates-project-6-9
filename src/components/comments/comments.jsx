e22d9db3ce27769c466ecb7c29b0e21b889b8f5e
hyejeong <withoo0414@gmail.com>
2022-02-23 14:04:24 +0900

    Fix error

--- src/components/comments/comments.jsx
+++ src/components/comments/comments.jsx
@@ -1,62 +1,68 @@
-import React, { useState, useRef } from "react"
-import Comment from "./section/comment"
-import styled from "styled-components"
+import React, { useState, useRef } from "react";
+import Comment from "./section/comment";
+import styled from "styled-components";
 
-import { useDispatch, useSelector } from "react-redux"
-import { addComment } from "../../reducers/commentReducer"
+import { useDispatch, useSelector } from "react-redux";
+import { addComment } from "../../reducers/reviewReducer";
 
-const Comments = (props) => {
-  const { comments } = useSelector((state) => state.comments)
-  const dispatch = useDispatch()
+const Comments = props => {
+  const { comments } = useSelector(state => state.reviews);
+  const dispatch = useDispatch();
 
-  console.log(comments)
+  console.log(comments);
 
-  const [text, setText] = useState("")
-  const [id, setId] = useState(1)
-  const inputRef = useRef()
+  const [text, setText] = useState("");
+  const inputRef = useRef();
 
-  const setTextValue = (e) => {
-    console.log(e.target.value)
-    setText(e.target.value)
-  }
+  const setTextValue = e => {
+    console.log(e.target.value);
+    setText(e.target.value);
+  };
 
-  const onSubmit = (e) => {
-    e.preventDefault()
+  const onSubmit = e => {
+    e.preventDefault();
     if (text === "") {
-      alert("내용을 입력해주세요.")
+      alert("내용을 입력해주세요.");
     } else {
       let data = {
-        id,
         content: text,
-        reviewId: "user1",
-      }
-      let result = dispatch(addComment(data))
-      console.log(result)
-      setId((prev) => prev + 1)
+        reviewId: "user1"
+      };
+      let result = dispatch(addComment(data));
+      console.log(result);
     }
-    setText("")
-  }
+    setText("");
+  };
 
   return (
     <CommentsComponent>
-      {comments && comments.map((comment, index) => <Comment comment={comment} key={index} />)}
+      {comments &&
+        comments.map((comment, index) =>
+          <Comment comment={comment} key={index} />
+        )}
       <FormComponent onSubmit={onSubmit}>
-        <input ref={inputRef} type="text" value={text} onChange={setTextValue} placeholder="댓글을 입력해주세요." />
+        <input
+          ref={inputRef}
+          type="text"
+          value={text}
+          onChange={setTextValue}
+          placeholder="댓글을 입력해주세요."
+        />
         <SubmitButton type="submit" onSubmit={onSubmit}>
           게시
         </SubmitButton>
       </FormComponent>
     </CommentsComponent>
-  )
-}
+  );
+};
 
-export default Comments
+export default Comments;
 
 const CommentsComponent = styled.div`
   margin-top: 30px;
   padding: 1.2rem 1.6rem;
   background: #f9f9f9;
-`
+`;
 
 const FormComponent = styled.form`
   width: 100%;
@@ -80,7 +86,7 @@ const FormComponent = styled.form`
       outline: none;
     }
   }
-`
+`;
 
 const SubmitButton = styled.button`
   font-size: 1.3rem;
@@ -94,4 +100,4 @@ const SubmitButton = styled.button`
   border-top: 1px solid #ddd;
   border-right: 1px solid #ddd;
   border-bottom: 1px solid #ddd;
-`
+`;
