dd90f05bba68f7259fe34a1f54d34fd3c668310d
madstone-dev <hanbin8570@gmail.com>
2022-02-23 14:16:52 +0900

    Fix : 시리얼라이즈 체크 옵션을 해제했습니다.

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reviewReducer from "./reducers/reviewReducer";
 
 export const store = configureStore({
   reducer: {
     reviews: reviewReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
