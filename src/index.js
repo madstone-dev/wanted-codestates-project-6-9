f6b4dd2a04fa276d6a4e4d411834c9dc7401b9d7
madstone-dev <hanbin8570@gmail.com>
2022-02-23 23:06:51 +0900

    Fix : basename 수정

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
-import { store } from "./store"
-import { Provider } from "react-redux"
-import { BrowserRouter } from "react-router-dom"
-import "./reset.css"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { store } from './store';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import './reset.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
-)
+  document.getElementById('root'),
+);
