339ea4da643a1e92bc3d04721ffee45ad26b5150
doyeon <doyeon311@gmail.com>
2022-02-23 09:46:31 +0900

    Feat: 뷰타입 변경하기

--- src/pages/ReviewList.js
+++ src/pages/ReviewList.js
@@ -1,16 +1,22 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import Filter from "../components/Filter";
-import GridList from "../components/GridList";
-import Modal from "../components/Modal";
-import TypeSelector from "../components/TypeSelector";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Filter from '../components/Filter';
+import GridType from '../components/GridType';
+import Modal from '../components/Modal';
+import TypeSelector from '../components/TypeSelector';
+import ListType from '../components/ListType';
 
 const ReviewList = () => {
   const [modalView, setModalView] = useState(false);
   const [selectedItem, setSelectedItem] = useState({
-    id: "recent",
-    text: "최신순",
+    id: 'recent',
+    text: '최신순',
   });
+  const [viewType, setViewType] = useState('grid');
+
+  const changeType = (type) => {
+    setViewType(type);
+  };
 
   const handleModal = () => {
     setModalView((prev) => !prev);
@@ -19,8 +25,8 @@ const ReviewList = () => {
     <Container>
       <ContentContainer>
         <Filter handleModal={handleModal} selectedItem={selectedItem} />
-        <TypeSelector />
-        <GridList />
+        <TypeSelector viewType={viewType} changeType={changeType} />
+        {viewType === 'grid' ? <GridType /> : <ListType />}
       </ContentContainer>
       {modalView && (
         <Modal
@@ -37,21 +43,25 @@ export default ReviewList;
 
 const Container = styled.div`
   width: 100vw;
-  height: 100vh;
   display: flex;
   justify-content: center;
   position: relative;
   background-color: #f9f9f9;
+  overflow: hidden;
 `;
 
 const ContentContainer = styled.div`
   background-color: white;
   position: relative;
   width: 550px;
-  /* height: 100vh; */
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px; ;
+  height: 100vh;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  overflow: scroll;
+
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
