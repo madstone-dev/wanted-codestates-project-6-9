6d1e72940d6a33af59fd439c2f16effabc2c1121
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2022-02-23 19:34:47 +0900

    Refactor: 폴더 및 파일명 정리

--- src/components/ListType.js
+++ src/components/ListType.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import styled from "styled-components";
+import React from "react"
+import { useSelector } from "react-redux"
+import styled from "styled-components"
 
-import InfiniteScroll from "./InfiniteScroll";
-import ProductsCard from "./productCard/ProductsCard";
+import InfiniteScroll from "./InfiniteScroll"
+import ProductsCard from "./ProductsCard"
 const GridList = ({ reviews }) => {
-  const maxPage = useSelector((state) => state.reviews.maxPage);
-  const page = useSelector((state) => state.reviews.page);
+  const maxPage = useSelector((state) => state.reviews.maxPage)
+  const page = useSelector((state) => state.reviews.page)
 
-  console.log(reviews);
+  console.log(reviews)
   return (
     <>
       <Container>
@@ -18,12 +18,12 @@ const GridList = ({ reviews }) => {
       </Container>
       {reviews.length > 0 && maxPage !== page && <InfiniteScroll />}
     </>
-  );
-};
+  )
+}
 
-export default GridList;
+export default GridList
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-`;
+`
