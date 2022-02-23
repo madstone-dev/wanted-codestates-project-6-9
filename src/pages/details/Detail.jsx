c4e146fc2f5a62dda3c29a7cb1c233b3efa212cf
hyejeong <withoo0414@gmail.com>
2022-02-23 12:58:58 +0900

    Feat: add infiniteScroll function
    디테일 컴포넌트에 무한스크롤 이벤트 추가

--- src/pages/details/Detail.jsx
+++ src/pages/details/Detail.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ProductsCard from "../../components/productCard/ProductsCard";
 import Header from "../../components/header/Header";
 import styled from "styled-components";
 import Scrolltotop from "../../common/scrollToTop";
 
 const Detail = () => {
+  const [loading, setLoading] = useState(false);
+  const [currentItems, setCurrentItems] = useState(1);
+  const end = useRef();
+
+  useEffect(
+    () => {
+      if (loading) {
+        const observer = new IntersectionObserver(
+          entries => {
+            if (entries[0].isIntersecting) {
+              loadMore();
+            }
+          },
+          { threshold: 1 }
+        );
+        observer.observe(end.current);
+      }
+    },
+    [loading]
+  );
+
+  const loadMore = () => {
+    setCurrentItems(prev => prev + 1);
+  };
+
   return (
     <DetailContainer>
       <DetailWrapper>
@@ -16,6 +41,7 @@ const Detail = () => {
         </ProductsContainer>
       </DetailWrapper>
       <Scrolltotop />
+      <div ref={end} />
     </DetailContainer>
   );
 };
@@ -23,14 +49,12 @@ const Detail = () => {
 const DetailContainer = styled.div`
   display: flex;
   width: 100%;
-
   background-color: #f9f9f9;
   overflow: hidden;
 `;
 
 const DetailWrapper = styled.div`
   margin: auto;
-
   background-color: #fff;
 `;
 
