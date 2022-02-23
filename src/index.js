a75fcf5113959e56509a7372e52166979e9bcf2b
madstone-dev <hanbin8570@gmail.com>
2022-02-23 23:14:38 +0900

    Fix: gh-pages를 위해 해시 라우터로 변경합니다.

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { store } from './store';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './reset.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
